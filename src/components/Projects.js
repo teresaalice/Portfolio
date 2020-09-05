3956a4c544ec404aabecd705fda2a171ec27df01
Teresa Alice <teresa.alice.alves@gmail.com>
2020-09-04 19:32:08 -0700

    Updated Projects component to stateless syntax

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,33 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import PROJECTS from "../data/projects";
 
-class Project extends Component {
-  render() {
-    const { title, image, description, link } = this.props.project;
-    return (
-      <div style={{ display: "inline-block", width: 400, margin: 10 }}>
-        <h3>{title}</h3>
-        <img src={image} alt="profile" style={{ width: 200 }} />
-        <p>{description}</p>
-        <a href={link}>Click me</a>
-      </div>
-    );
-  }
-}
+const Project = (props) => {
+  const { title, image, description, link } = props.project;
+  return (
+    <div style={{ display: "inline-block", width: 400, margin: 10 }}>
+      <h3>{title}</h3>
+      <img src={image} alt="profile" style={{ width: 200 }} />
+      <p>{description}</p>
+      <a href={link}>Click me</a>
+    </div>
+  );
+};
 
-class Projects extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Highlighted Projects</h2>
-        <div>
-          {PROJECTS.map((PROJECT) => {
-            return <Project key={PROJECT.id} project={PROJECT} />;
-          })}
-        </div>
-      </div>
-    );
-  }
-}
+const Projects = () => (
+  <div>
+    <h2>Highlighted Projects</h2>
+    <div>
+      {PROJECTS.map((PROJECT) => {
+        return <Project key={PROJECT.id} project={PROJECT} />;
+      })}
+    </div>
+  </div>
+);
 
 export default Projects;
